9782792a6a962030971ffe9622197df57e6d39c6
Ben <ben.lynch1931@gmail.com>
2020-12-01 11:10:49 +0000

    style: format test file using babel-eslint

--- spec/BankAccount.test.js
+++ spec/BankAccount.test.js
@@ -1,43 +1,45 @@
-import BankAccount from '../src/BankAccount';
-import PrintStatement from '../src/PrintStatement';
+import BankAccount from "../src/BankAccount";
+import PrintStatement from "../src/PrintStatement";
 
-describe('BankAccount', () => {
-  var account;
+describe("BankAccount", () => {
+  let account;
 
   beforeEach(() => {
-    account = new BankAccount;
-  })
+    account = new BankAccount();
+  });
 
-  test('is instantiated', () => {
-    expect(account).toBeInstanceOf(BankAccount)
-  })
+  test("is instantiated", () => {
+    expect(account).toBeInstanceOf(BankAccount);
+  });
 
+  describe(" #credit", () => {
+    test("accountActivity updates with date and amount of credit", () => {
+      expect(account.credit(1000, "10/01/2012")).toEqual([{
+        credit: 1000, debit: 0, date: "10/01/2012", newBalance: 1000,
+      }]);
+    });
+  });
 
-  describe(' #credit', () => {
+  describe(" #debit", () => {
+    test("accountActivity updates with date and amount of debit", () => {
+      account.credit(500, "14/01/2012");
+      expect(account.debit(500, "14/01/2012")).toEqual([{
+        credit: 500, debit: 0, date: "14/01/2012", newBalance: 500,
+      }, {
+        credit: 0, debit: 500, date: "14/01/2012", newBalance: 0,
+      }]);
+    });
 
-    test('accountActivity updates with date and amount of credit', () => {
-      expect(account.credit(1000, '10/01/2012')).toEqual([{'credit': 1000, 'debit': 0, 'date': '10/01/2012', 'newBalance': 1000}])
-    })
-  })
+    test("owner cannot debit more than current balance", () => {
+      expect(account.debit(500, "14/01/2012")).toEqual("Error: insufficient funds, please review your balance.");
+    });
+  });
 
-  describe(' #debit', () => {
-
-    test('accountActivity updates with date and amount of debit', () => {
-      account.credit(500, '14/01/2012')
-      expect(account.debit(500, '14/01/2012')).toEqual([{'credit': 500, 'debit': 0, 'date': '14/01/2012', 'newBalance': 500}, {'credit': 0, 'debit': 500, 'date': '14/01/2012', 'newBalance': 0}])
-    })
-
-    test('owner cannot debit more than current balance', () => {
-      expect(account.debit(500, '14/01/2012')).toEqual('Error: insufficient funds, please review your balance.')
-    })
-  })
-
-  describe(' #print', () => {
-
-    test('method calls PrintStatement methods', () => {
-      const printing = jest.spyOn(PrintStatement, 'printAll')
-      account.print()
-      expect(printing).toHaveBeenCalled()
-    })
-  })
-})
+  describe(" #print", () => {
+    test("method calls PrintStatement methods", () => {
+      const printing = jest.spyOn(PrintStatement, "printAll");
+      account.print();
+      expect(printing).toHaveBeenCalled();
+    });
+  });
+});
--- spec/PrintStatement.test.js
+++ spec/PrintStatement.test.js
@@ -1,58 +1,61 @@
-import PrintStatement from '../src/PrintStatement';
+import PrintStatement from "../src/PrintStatement";
 
-
-describe('PrintStatement', () => {
-  var consoleLogSpy
+describe("PrintStatement", () => {
+  let logSpy;
 
   beforeEach(() => {
-    consoleLogSpy = jest.spyOn(console, 'log')
-  })
+    logSpy = jest.spyOn(console, "log");
+  });
 
   afterEach(() => {
-    consoleLogSpy.mockClear();
-  })
+    logSpy.mockClear();
+  });
 
-  describe(' #printHeader', () => {
+  describe(" #printHeader", () => {
+    test(" it prints PrintStatement headers", () => {
+      PrintStatement.printHeader();
+      const consoleLogSpy = logSpy.mock.calls;
+      expect(consoleLogSpy[0][0]).toEqual("date || credit || debit || balance");
+    });
+  });
 
-    test(' it prints PrintStatement headers', () => {
-      PrintStatement.printHeader()
-      expect(consoleLogSpy.mock.calls[0][0]).toEqual("date || credit || debit || balance")
-    })
-  })
+  describe(" #printInfo", () => {
+    test(" it prints each credit", () => {
+      PrintStatement.printInfo({
+        credit: 500, debit: 0, date: "14/01/2012", newBalance: 500,
+      });
+      expect(logSpy.mock.calls[0][0]).toEqual("14/01/2012 || 500 ||  || 500");
+    });
 
-  describe(' #printInfo', () => {
+    test(" is prints each debit", () => {
+      PrintStatement.printInfo({
+        credit: 0, debit: 100, date: "14/11/2012", newBalance: -100,
+      });
+      expect(logSpy.mock.calls[0][0]).toEqual("14/11/2012 ||  || 100 || -100");
+    });
+  });
 
-    test(' it prints each credit', () => {
-      PrintStatement.printInfo({credit: 500, debit: 0, date: '14/01/2012', newBalance: 500})
-      expect(consoleLogSpy.mock.calls[0][0]).toEqual("14/01/2012 || 500 ||  || 500")
-    })
-
-    test(' is prints each debit', () => {
-      PrintStatement.printInfo({credit: 0, debit: 100, date: '14/11/2012', newBalance: -100})
-      expect(consoleLogSpy.mock.calls[0][0]).toEqual("14/11/2012 ||  || 100 || -100")
-    })
-  })
-
-  describe(' #isZero', () => {
-
-    test(' it returns empty string when value is zero', () => {
-      expect(PrintStatement.isZero(0)).toEqual('')
-    })
+  describe(" #isZero", () => {
+    test(" it returns empty string when value is zero", () => {
+      expect(PrintStatement.isZero(0)).toEqual("");
+    });
 
     test(" it returns same value when value isn't zero", () => {
-      expect(PrintStatement.isZero(100)).toEqual(100)
-    })
-  })
+      expect(PrintStatement.isZero(100)).toEqual(100);
+    });
+  });
 
-  describe(' #printAll', () => {
-
-
-
-    test(' it iterates the activity array and prints each credit/debit', () => {
-      PrintStatement.printAll([{date: '10/01/2012', credit: 1000, debit: 0, newBalance: 1000}, {date: '14/01/2012', credit: 0, debit: 300, newBalance: 700}])
-      expect(consoleLogSpy.mock.calls[0][0]).toEqual("date || credit || debit || balance")
-      expect(consoleLogSpy.mock.calls[2][0]).toEqual("10/01/2012 || 1000 ||  || 1000")
-      expect(consoleLogSpy.mock.calls[1][0]).toEqual("14/01/2012 ||  || 300 || 700")
-    })
-  })
-})
+  describe(" #printAll", () => {
+    test(" it iterates the activity array and prints each credit/debit", () => {
+      PrintStatement.printAll([{
+        date: "10/01/2012", credit: 1000, debit: 0, newBalance: 1000,
+      }, {
+        date: "14/01/2012", credit: 0, debit: 300, newBalance: 700,
+      }]);
+      const consoleLogSpy = logSpy.mock.calls;
+      expect(consoleLogSpy[0][0]).toEqual("date || credit || debit || balance");
+      expect(consoleLogSpy[2][0]).toEqual("10/01/2012 || 1000 ||  || 1000");
+      expect(consoleLogSpy[1][0]).toEqual("14/01/2012 ||  || 300 || 700");
+    });
+  });
+});
